7876bd6d61d6a4c9abae8c90d3f0679658994d4e
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-04-10 12:45:38 -0300

    Creating new model (mongoose.Schema) and organizing project structure.

---
+++ src/models/Book.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+var BookSchema = new Schema({
+    title: String,
+    authot: String,
+    category: String
+});
+
+module.exports = mongoose.model('Book', BookSchema);
