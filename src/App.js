8dc3dfb6df322ef55b0177983d3cde66066f6d0a
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2020-04-10 12:37:26 -0300

    Setting up environment variables, using dotenv.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser= require('body-parser');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-const PORT = 8080;
+dotenv.config();
+const PORT = process.env.PORT;
 const app = express();
+
 
 app.listen(PORT, () =>{
     console.log(`App listening on port ${PORT}`)
